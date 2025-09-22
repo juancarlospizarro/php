fe8f1b1057904278a5695ee84bc47d3a3228907c
juancarlospizarro <jcpizarroalonso@gmail.com>
2025-09-22 10:48:51 +0200

    Ejercicio 3 realizado

--- actividades/u1/ejercicio1.php
+++ actividades/u1/ejercicio1.php
@@ -7,7 +7,40 @@
   $direccion = "Calle Fernando Cepeda 35";
   $telefono = "612345789";
   echo "Mi nombre es $nombre"."<br>"."Mi direccion es $direccion"."<br>"."Mi telefono es $telefono";
+  $palabra1ingles = "Room";
+  $palabra1castellano = "Habitación";
+  $palabra2ingles = "Red";
+  $palabra2castellano = "Rojo";
+  $palabra3ingles = "Kitchen";
+  $palabra3castellano = "Cocina";
+  $palabra4ingles = "Garden";
+  $palabra4castellano = "Jardín";
+  $palabra5ingles = "Window";
+  $palabra5castellano = "Ventana";
+  $palabra6ingles = "Chair";
+  $palabra6castellano = "Silla";
+  $palabra7ingles = "Table";
+  $palabra7castellano = "Mesa";
+  $palabra8ingles = "Bottle";
+  $palabra8castellano = "Botella";
+  $palabra9ingles = "Screen";
+  $palabra9castellano = "Pantalla";
+  $palabra10ingles = "Keyword";
+  $palabra10castellano = "Teclado";
 
+  echo "<table border='1'";
+  echo "<tr><th>Inglés</th><th>Español</th></tr>";
+  echo "<tr><td>$palabra1ingles</td><td>$palabra1castellano</td></tr>";
+  echo "<tr><td>$palabra2ingles</td><td>$palabra2castellano</td></tr>";
+  echo "<tr><td>$palabra3ingles</td><td>$palabra3castellano</td></tr>";
+  echo "<tr><td>$palabra4ingles</td><td>$palabra4castellano</td></tr>";
+  echo "<tr><td>$palabra5ingles</td><td>$palabra5castellano</td></tr>";
+  echo "<tr><td>$palabra6ingles</td><td>$palabra6castellano</td></tr>";
+  echo "<tr><td>$palabra7ingles</td><td>$palabra7castellano</td></tr>";
+  echo "<tr><td>$palabra8ingles</td><td>$palabra8castellano</td></tr>";
+  echo "<tr><td>$palabra9ingles</td><td>$palabra9castellano</td></tr>";
+  echo "<tr><td>$palabra10ingles</td><td>$palabra10castellano</td></tr>";
+  echo "</table>";
 ?>
 </body>
 </html>
