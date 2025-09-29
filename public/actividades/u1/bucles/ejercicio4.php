b645d7a18b09aae57508bfbd762471ac96b2b922
juancarlospizarro <jcpizarroalonso@gmail.com>
2025-09-29 10:33:04 +0200

    Ejercicio 4 de bucles realizado

---
+++ public/actividades/u1/bucles/ejercicio4.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 4</title>
+</head>
+<body>
+    <?php
+    echo "<h1>Bucle descendente de 320 a 160 usando un bucle For</h1>";
+    echo "<hr>";
+    for($i = 320; $i >= 160; $i -= 20){
+        echo "El valor de i es $i <br>";
+    }
+
+    ?>
+</body>
+</html>
